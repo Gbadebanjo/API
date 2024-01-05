16307332ee56f78609fa2d3aef65372ab58d1252
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2024-01-05 23:53:51 +0100

    Initiated Product page data

---
+++ src/model/product.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+/* Product Interface */
+interface IProduct extends Document {
+  name: string;
+  picture: string;
+  price: number;
+  description: string;
+}
+
+/* Product Schema */
+const ProductSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    picture: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model<IProduct>("Product", ProductSchema);
+
+export default Product;
+export { IProduct };
+
+
+
+
+
+
+
