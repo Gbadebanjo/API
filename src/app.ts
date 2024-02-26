cd87a23070c895dd8ee132c57ca2ee24d6443e97
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2024-02-26 18:35:24 +0100

    Added origin details

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,56 @@
-import { config } from 'dotenv';
-import createError from 'http-errors';
-import express, { Request, Response, NextFunction } from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import cors from 'cors';
+import { config } from "dotenv";
+import createError from "http-errors";
+import express, { Request, Response, NextFunction } from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import cors from "cors";
 
 config();
 
-import indexRouter from './routes/index';
-import  usersRouter from './routes/users'; 
-import adminRouter from './routes/admin'; 
-import  cloudinaryApp  from './utility/cloudinary'; 
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
+import adminRouter from "./routes/admin";
+import cloudinaryApp from "./utility/cloudinary";
 // import from 'express';
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "ejs");
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://oba-soles.vercel.app"],
+    credentials: true,
+  })
+);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 
-
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/admin', adminRouter);
-app.use('/cloudinary', cloudinaryApp);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/admin", adminRouter);
+app.use("/cloudinary", cloudinaryApp);
 
 // catch 404 and forward to error handler
-app.use(function(req: Request, res: Response, next: NextFunction) {
+app.use(function (req: Request, res: Response, next: NextFunction) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err: any, req: Request, res: Response, next: NextFunction) {
+app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 export default app;
