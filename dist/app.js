be38ed30353eb0004c62f4538feea4a298aff3e0
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2024-02-26 19:57:52 +0100

    Updated origin TS

--- dist/app.js
+++ dist/app.js
@@ -18,21 +18,21 @@ const cloudinary_1 = __importDefault(require("./utility/cloudinary"));
 // import from 'express';
 const app = (0, express_1.default)();
 // view engine setup
-app.set('views', path_1.default.join(__dirname, '../views'));
-app.set('view engine', 'ejs');
+app.set("views", path_1.default.join(__dirname, "../views"));
+app.set("view engine", "ejs");
 app.use((0, cors_1.default)({
-    origin: 'http://localhost:3000',
-    credentials: true
+    origin: ["http://localhost:3000", "https://oba-soles.vercel.app"],
+    credentials: true,
 }));
-app.use((0, morgan_1.default)('dev'));
+app.use((0, morgan_1.default)("dev"));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cookie_parser_1.default)());
-app.use(express_1.default.static(path_1.default.join(__dirname, '../public')));
-app.use('/', index_1.default);
-app.use('/users', users_1.default);
-app.use('/admin', admin_1.default);
-app.use('/cloudinary', cloudinary_1.default);
+app.use(express_1.default.static(path_1.default.join(__dirname, "../public")));
+app.use("/", index_1.default);
+app.use("/users", users_1.default);
+app.use("/admin", admin_1.default);
+app.use("/cloudinary", cloudinary_1.default);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next((0, http_errors_1.default)(404));
@@ -41,9 +41,9 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = req.app.get("env") === "development" ? err : {};
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render("error");
 });
 exports.default = app;
